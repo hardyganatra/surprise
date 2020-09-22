fbac7cf76efffb156ff02e3bcc7267c58a8ca339
HARDIK GANATRA <HARDIK.GANATRA@lntinfotech.com>
2020-09-22 21:34:28 +0530

    minnion in place

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
+import minnion from "./Assets/minnion.gif";
+import minnionbd from "./Assets/minnion2bd.gif";
 function App() {
-	return <div className="App">hardik</div>;
+	return (
+		<div className="App">
+			<div className="intropage">
+				<img
+					className="minnionintro"
+					src={minnion}
+					alt="..loading"
+				></img>
+			</div>
+			<h3 className="wishh3">Happyyyyyyyyyy Birthday Foruuuuuu</h3>
+		</div>
+	);
 }
 
 export default App;
