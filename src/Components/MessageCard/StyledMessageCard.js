c81f27d44e6df92098804f468be06f3863d80ce1
Hardik Ganatra <hardik.ganatra@lntinfotech.com>
2020-09-25 00:00:46 +0530

    Component FOlder

---
+++ src/Components/MessageCard/StyledMessageCard.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import Foruu from "./Foruu.jpg";
+export const StyledMessageCard = styled.div`
+	width: 100%;
+	font-size: 18px;
+	border: 3px solid blue;
+	font-family: Verdana, Geneva, Tahoma, sans-serif;
+	background-color: bisque;
+	line-height: 25px;
+`;
---
+++ src/Components/MessageCard/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { StyledMessageCard } from "./StyledMessageCard";
+
+export default function MessageCard({ message }) {
+	return <StyledMessageCard>{message}</StyledMessageCard>;
+}
