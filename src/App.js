fecec9e380e00891dfc2687f762532c2d55c2ca6
Hardik Ganatra <hardik.ganatra@lntinfotech.com>
2020-09-24 20:48:55 +0530

    Messagebox Added

--- src/App.js
+++ src/App.js
@@ -19,6 +19,28 @@ function App() {
 			<div className="coverpic">
 				<img className="coverpicimg" src={Foruu} alt="loading"></img>
 			</div>
+			<div className="MessageCard">
+				It is a long established fact that a reader will be distracted
+				by the readable content of a page when looking at its layout.
+				The point of using Lorem Ipsum is that it has a more-or-less
+				normal distribution of letters, as opposed to using 'Content
+				here, content here', making it look like readable English. Many
+				desktop publishing packages and web page editors now use Lorem
+				Ipsum as their default model text, and a search for 'lorem
+				ipsum' will uncover many web sites still in their infancy.
+				Various versions have evolved over the years, sometimes by
+				accident, sometimes on purpose (injected humour and the like) It
+				is a long established fact that a reader will be distracted by
+				the readable content of a page when looking at its layout. The
+				point of using Lorem Ipsum is that it has a more-or-less normal
+				distribution of letters, as opposed to using 'Content here,
+				content here', making it look like readable English. Many
+				desktop publishing packages and web page editors now use Lorem
+				Ipsum as their default model text, and a search for 'lorem
+				ipsum' will uncover many web sites still in their infancy.
+				Various versions have evolved over the years, sometimes by
+				accident, sometimes on purpose (injected humour and the like)
+			</div>
 		</div>
 	);
 }
